dcf068b126550a050fb618f22d139f61d3e24c61
daygle <daygle@daygle.net>
2026-06-26 21:43:00 +1000

    chore: upgrade AGP to 9.2.1 and Hilt to 2.60, and configure toolchain detection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Hilt DI
-    implementation("com.google.dagger:hilt-android:2.54")
-    ksp("com.google.dagger:hilt-compiler:2.54")
+    implementation("com.google.dagger:hilt-android:2.60")
+    ksp("com.google.dagger:hilt-compiler:2.60")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("androidx.hilt:hilt-work:1.2.0")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
