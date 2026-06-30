e5cea8c33dc806dade1c89b22889e4f1ccc380bf
daygle <daygle@daygle.net>
2026-06-30 22:15:36 +1000

    \Increment Room database version to 3 to fix schema mismatch"

--- app/src/main/java/com/locapeer/data/AppDatabase.kt
+++ app/src/main/java/com/locapeer/data/AppDatabase.kt
@@ -18,7 +18,7 @@ import com.locapeer.data.entity.*
         PendingMessageEntity::class,
         PendingRequestEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
